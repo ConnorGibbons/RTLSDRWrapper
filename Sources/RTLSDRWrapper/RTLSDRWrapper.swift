2129dfa6e6d88a7014af840d414189ba8326e22a
Connor Gibbons <connorgibbons@comcast.net>
2025-04-16 10:48:14 -0400

    Bruh

--- Sources/RTLSDRWrapper/RTLSDRWrapper.swift
+++ Sources/RTLSDRWrapper/RTLSDRWrapper.swift
@@ -1,10 +1,7 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
 import CRTLSDR
 
 public enum SDRProbe {
-    func getDeviceCount() -> UInt32 {
+    static func getDeviceCount() -> UInt32 {
         return CRTLSDR.rtlsdr_get_device_count()
     }
 }
